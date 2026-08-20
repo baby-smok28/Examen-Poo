b225ebcedde657ec6c80b2ea15c9decbea2dd7b3
baby-smok28 <hei.faneva@gmail.com>
2026-08-20 13:29:19 +0300

    Initial commit - Examen Final Poo (Spring Boot 4.1.0)

---
+++ src/main/java/org/example/examenfinalpoo/ExamenFinalPooApplication.java
@@ -0,0 +1,13 @@
+package org.example.examenfinalpoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalPooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFinalPooApplication.class, args);
+    }
+
+}
